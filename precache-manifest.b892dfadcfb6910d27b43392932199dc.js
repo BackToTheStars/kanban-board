997a96a95d4e4e748cacb679aa28c5053e9555a2
BackToTheStars <nikgrigoryev@gmail.com>
2020-06-08 21:02:48 -0700

    Updates

---
+++ precache-manifest.b892dfadcfb6910d27b43392932199dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65f99d391425eee117de88d24514fdcc",
+    "url": "/kanban-board/index.html"
+  },
+  {
+    "revision": "aa9f2eb36e69881b01ee",
+    "url": "/kanban-board/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "616ec0ba37284066ad17",
+    "url": "/kanban-board/static/css/main.4afce826.chunk.css"
+  },
+  {
+    "revision": "aa9f2eb36e69881b01ee",
+    "url": "/kanban-board/static/js/2.5c3db0c8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kanban-board/static/js/2.5c3db0c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "616ec0ba37284066ad17",
+    "url": "/kanban-board/static/js/main.bbafd55c.chunk.js"
+  },
+  {
+    "revision": "df7f5190008ec2303916",
+    "url": "/kanban-board/static/js/runtime-main.6be55f1a.js"
+  }
+]);
